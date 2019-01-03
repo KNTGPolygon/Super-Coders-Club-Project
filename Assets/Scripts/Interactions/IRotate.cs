3e3e8e15ef00e1a0f80271ff1821b35c88b2431b
koniczyn5 <koniczyn5@tlen.pl>
2019-01-03 19:00:13 +0100

    Guard+Interactions update
    -Implementation of guards auto-patrolling system
    -Small update in interactions, adding rotation scripts

---
+++ Assets/Scripts/Interactions/IRotate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRotate{
+    Vector3 direction { set; }
+    void DoRotate();
+    void SetSpeed(float speed); //degrees per second
+	
+}
---
+++ Assets/Scripts/Interactions/RightRotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightRotate : MonoBehaviour, IRotate {
+
+    public Vector3 direction { private get; set; }
+    private Coroutine coroutine;
+    private float speed=45;
+    public void Start()
+    {
+        direction = Vector3.back;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public void DoRotate()
+    {
+        coroutine = this.StartCoroutine(DoingRotate());
+    }
+
+    private IEnumerator DoingRotate()
+    {
+        float timer = 0;
+        while (timer < 90/speed)
+        {
+            transform.Rotate(direction * speed * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
