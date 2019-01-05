f8b4a28c0e093d88097adac9051aaeb60befb05c
koniczyn5 <koniczyn5@tlen.pl>
2019-01-05 17:59:39 +0100

    Player control
    -Added player control

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	[SerializeField] private float playerSpeedPerSecond;
+
+	void Update()
+	{
+		UpdatedPosition("d", Vector3.right);
+		UpdatedPosition("a", Vector3.left);
+		UpdatedPosition("w", Vector3.up);
+		UpdatedPosition("s", Vector3.down);
+	}
+
+	private void UpdatedPosition(string key ,Vector3 moveVector)
+	{	
+		if (Input.GetKey(key))
+		{
+			transform.position += moveVector * playerSpeedPerSecond * Time.deltaTime;
+		}
+	}
+}
