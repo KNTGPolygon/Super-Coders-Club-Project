e640adde4c73294ec3d9e7fb8f1a7a3744c096e3
koniczyn5 <koniczyn5@tlen.pl>
2019-01-10 19:29:21 +0100

    Map build
    -Map was builded
    -Camera follows Player
    -part of the game ending mechanism

---
+++ Assets/Scripts/EndGame/GameLose.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLose : MonoBehaviour, IEndGame
+{
+
+	[SerializeField] private GameObject gameOverObject;
+
+	public void EndGame()
+	{
+		gameOverObject.SetActive(true);
+		this.StartCoroutine(DeathDisplay());
+	}
+
+	private IEnumerator DeathDisplay()
+	{
+		//Dopoki nie wcisnieta ktoregos buttona nic nie rob
+		yield return null;
+	}
+}
